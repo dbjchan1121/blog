822d6982ae5a8a7bfa6545c8a4ab8db78a6eaf67
陈喆 <307400468@qq.com>
2021-08-05 10:26:50 +0800

    侧边栏配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	lang: 'zh-CN',
+	title: '小陳同学',
+	description: '日常瞎扯',
+	themeConfig: {
+		logo: 'https://vuejs.org/images/logo.png',
+		sidebar: [
+			{
+				text: 'JS',
+				link: '/JS/',
+				children: [
+					{
+						text: 'this',
+						link: 'this.md'
+					}
+				]
+			}
+		]
+	}
+}
