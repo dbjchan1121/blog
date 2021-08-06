53df8c69acee2fdaec870fd4688d2d96cbe693aa
陈喆 <307400468@qq.com>
2021-08-06 10:08:06 +0800

    配置navbar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,18 +3,50 @@ module.exports = {
 	title: '小陳同学',
 	description: '日常瞎扯',
 	themeConfig: {
-		logo: 'https://vuejs.org/images/logo.png',
-		sidebar: [
+		logo: 'https://cdn0.iconfinder.com/data/icons/hexagone-business-finance-set-1/128/BLOG_WRITING-512.png',
+		navbar: [
 			{
-				text: 'JS',
-				link: '/JS/',
+				text: 'Linux/OS X',
+				link: '/system/'
+			},
+			{
+				text: '读书笔记',
 				children: [
 					{
-						text: 'this',
-						link: 'this.md'
+						text: '你不知道的JS（上）',
+						link: '/reading/You-Dont-Know-JS-1st'
 					}
 				]
+			},
+			{
+				text: '源码阅读',
+				children: [
+					{
+						text: 'vue 2.x',
+						link: '/analysis/vue2.x'
+					},
+					{
+						text: 'vue 3.x',
+						link: '/analysis/vue3.x'
+					}
+				]
+			},
+			{
+				text: 'Github',
+				link: 'https://github.com/dbjchan1121'
 			}
-		]
+		],
+		// sidebar: [
+		// 	{
+		// 		text: 'JS',
+		// 		link: '/JS/',
+		// 		children: [
+		// 			{
+		// 				text: 'this',
+		// 				link: 'this.md'
+		// 			}
+		// 		]
+		// 	}
+		// ]
 	}
 }
